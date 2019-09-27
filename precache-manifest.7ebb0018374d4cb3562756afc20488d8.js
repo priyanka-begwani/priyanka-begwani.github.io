e846c1dfffc2f62254d2be4cf245633b67cd224f
priyanka <begwani.priyanka5@gmail.com>
2019-09-27 16:28:57 +0530

    demo intial changes
    demo link

---
+++ precache-manifest.7ebb0018374d4cb3562756afc20488d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d249534ceadad6488dd2dcfcd4c4499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a9a046771726b1a1daf",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5883fede6f16ee2f7787",
+    "url": "/static/js/2.35cc5894.chunk.js"
+  },
+  {
+    "revision": "5a9a046771726b1a1daf",
+    "url": "/static/js/main.cd057a2b.chunk.js"
+  },
+  {
+    "revision": "fe694ba00cf2ffed738c",
+    "url": "/static/js/runtime-main.f7c21016.js"
+  }
+]);
